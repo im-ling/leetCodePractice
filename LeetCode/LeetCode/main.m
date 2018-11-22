520b172172dd46dda10f9a932e4abb36b1fdae94
DoubleL <tobeabetterman_dl@163.com>
2018-11-22 09:03:56 +0800

    setup environment

---
+++ LeetCode/LeetCode/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  LeetCode
+//
+//  Created by DoubleL on 2018/11/22.
+//  Copyright © 2018 DoubleL. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
