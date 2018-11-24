271f467fc4fc3c220d2f33bfb58b3eaa626eef72
DoubleL <tobeabetterman_dl@163.com>
2018-11-24 18:30:21 +0800

    //Q1:Two Sum

--- LeetCode/LeetCode/main.m
+++ LeetCode/LeetCode/main.m
@@ -12,6 +12,43 @@ int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
         NSLog(@"Hello, World!");
+//        int question1(void);
+//        question1();
     }
     return 0;
+}
+
+//Q1:Two Sum
+//Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+//You may assume that each input would have exactly one solution, and you may not use the same element twice.
+//Example:
+//Given nums = [2, 7, 11, 15], target = 9,
+//Because nums[0] + nums[1] = 2 + 7 = 9,
+//return [0, 1].
+
+int question1(){
+    printf("question1\n");
+    int nums[] = {2, 7, 11, 15};
+    int target = 9;
+    //    int *result = NULL;
+    int* twoSum(int* nums, int numsSize, int target);
+    int *result = twoSum(nums, 4, target);
+    printf("result = {%d , %d}",*result , * (result+1));
+    return 0;
+}
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* twoSum(int* nums, int numsSize, int target) {
+    int *resultArray = malloc(sizeof(int) * 2);
+    for(int i = 0; i < numsSize; i++){
+        for (int j = i+1; j < numsSize; j++) {
+            if (nums[i] == target - nums[j]) {
+                *resultArray = i;
+                *(resultArray+1) = j;
+                return resultArray;
+            }
+        }
+    }
+    return resultArray;
 }
