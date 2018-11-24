85fe9922c670ae4052c1ec123aa8dc55b0b239df
DoubleL <tobeabetterman_dl@163.com>
2018-11-24 18:31:43 +0800

    //70. Climbing Stairs

--- LeetCode/LeetCode/main.m
+++ LeetCode/LeetCode/main.m
@@ -16,6 +16,8 @@ int main(int argc, const char * argv[]) {
 //        question1();
 //        int question2(void);
 //        question2();
+//        int question70(void);
+//        question70();
     }
     return 0;
 }
@@ -124,5 +126,42 @@ int question2(void){
         result = result->next;
     }
     return 0;
+}
+
+//70. Climbing Stairs
+//You are climbing a stair case. It takes n steps to reach to the top.
+//Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+//Note: Given n will be a positive integer.
+//Example 1:
+//Input: 2
+//Output: 2
+//Explanation: There are two ways to climb to the top.
+//1. 1 step + 1 step
+//2. 2 steps
+//Example 2:
+//Input: 3
+//Output: 3
+//Explanation: There are three ways to climb to the top.
+//1. 1 step + 1 step + 1 step
+//2. 1 step + 2 steps
+//3. 2 steps + 1 step
+int question70(){
+    int step = 4;
+    int climbStairs(int n);
+    int result = climbStairs(step);
+    printf("climb %d,has %d way",step,result);
+    return 0;
+}
+
+int climbStairs(int n) {
+    int temp = 0;
+    int result = 1;
+    int lastResult = 0;
+    for (int i = 1; i <= n; i++) {
+        temp = lastResult;
+        lastResult = result;
+        result += temp;
+    }
+    return result;
 }
 
