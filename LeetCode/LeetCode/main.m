f2331173de2526bcf729ef397d19420291898a71
DoubleL <tobeabetterman_dl@163.com>
2018-11-24 18:31:24 +0800

    //2. Add Two Numbers

--- LeetCode/LeetCode/main.m
+++ LeetCode/LeetCode/main.m
@@ -14,6 +14,8 @@ int main(int argc, const char * argv[]) {
         NSLog(@"Hello, World!");
 //        int question1(void);
 //        question1();
+//        int question2(void);
+//        question2();
     }
     return 0;
 }
@@ -52,3 +54,75 @@ int* twoSum(int* nums, int numsSize, int target) {
     }
     return resultArray;
 }
+
+//2. Add Two Numbers
+//You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
+//You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+//Example:
+//Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+//Output: 7 -> 0 -> 8
+//Explanation: 342 + 465 = 807.
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+
+struct ListNode {
+    int val;
+    struct ListNode *next;
+};
+
+struct ListNode* newNodeWith(struct ListNode* l1, struct ListNode* l2, int *carryFlag){
+    struct ListNode *result = calloc(1, sizeof(struct ListNode));
+    if (l1 && l2) {
+        result->val = l1->val + l2->val + *carryFlag;
+    }else if(l1){
+        result->val = l1->val + *carryFlag;
+    }else if (l2){
+        result->val = l2->val + *carryFlag;
+    }else{
+        result->val = *carryFlag;
+    }
+    *carryFlag = result->val / 10;
+    result->val = result->val % 10;
+    return result;
+}
+struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2) {
+    struct ListNode *p1 = l1;
+    struct ListNode *p2 = l2;
+    struct ListNode *result = calloc(1, sizeof(struct ListNode));
+    struct ListNode *tempResult = result;
+    int carryFlag = 0;
+    while (p1 != NULL || p2 != NULL || carryFlag != 0) {
+        tempResult->next = newNodeWith(p1, p2, &carryFlag);
+        tempResult = tempResult->next;
+        if (p1) {
+            p1 = p1->next;
+        }
+        if (p2) {
+            p2 = p2->next;
+        }
+    }
+    tempResult = result->next;
+    free(result);
+    return tempResult;
+}
+int question2(void){
+    struct ListNode s11 = {3, NULL};
+    struct ListNode s12 = {8, &s11};
+    struct ListNode s13 = {1, &s12};
+    
+    struct ListNode s21 = {4, NULL};
+    struct ListNode s22 = {6, &s21};
+    struct ListNode s23 = {2, &s22};
+    struct ListNode *result = addTwoNumbers(&s13, &s23);
+    while (result) {
+        printf(" %d ",result->val);
+        result = result->next;
+    }
+    return 0;
+}
+
