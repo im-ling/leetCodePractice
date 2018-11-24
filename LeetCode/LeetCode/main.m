f25dc70f7cb6b8d0bb9afbbb9e474897397a623b
DoubleL <tobeabetterman_dl@163.com>
2018-11-24 21:11:50 +0800

    //4. Median of Two Sorted Arrays

--- LeetCode/LeetCode/main.m
+++ LeetCode/LeetCode/main.m
@@ -18,6 +18,8 @@ int main(int argc, const char * argv[]) {
 //        question2();
 //        int question3(void);
 //        question3();
+//        int question4(void);
+//        question4();
 //        int question70(void);
 //        question70();
     }
@@ -215,6 +217,62 @@ int question3(){
     return 0;
 };
 
+
+//4. Median of Two Sorted Arrays
+//Hard
+//There are two sorted arrays nums1 and nums2 of size m and n respectively.
+//Find the median of the two sorted arrays. The overall run time complexity should be O(log (m+n)).
+//You may assume nums1 and nums2 cannot be both empty.
+//Example 1:
+//nums1 = [1, 3]
+//nums2 = [2]
+//The median is 2.0
+//Example 2:
+//nums1 = [1, 2]
+//nums2 = [3, 4]
+//The median is (2 + 3)/2 = 2.5
+double findMedianSortedArrays(int* nums1, int nums1Size, int* nums2, int nums2Size) {
+    double result = 0;
+    int newNumberSize = nums1Size + nums2Size;
+    int *newNumbers = malloc(sizeof(int) * newNumberSize);
+    memset(newNumbers, 0, sizeof(int) * newNumberSize);
+    int *p = newNumbers;
+    int i = 0;
+    int j = 0;
+    while (i < nums1Size && j < nums2Size) {
+        if (nums1[i] < nums2[j]) {
+            *p++ = nums1[i++];
+        }else{
+            *p++ = nums2[j++];
+        }
+    }
+    if (i >= nums1Size) {
+        while (j < nums2Size) {
+            *p++ = nums2[j++];
+        }
+    }else{
+        while (i < nums1Size) {
+            *p++ = nums1[i++];
+        }
+    }
+    int index = newNumberSize / 2;
+    if (newNumberSize % 2) {
+        result = newNumbers[index];
+    }else{
+        result = newNumbers[index] + newNumbers[index - 1];
+        result = result / 2.0;
+    }
+    free(newNumbers);
+    return result;
+}
+
+int question4(){
+    int nums1[] = {1, 2};
+    int nums2[] = {3, 4};
+    double result = findMedianSortedArrays(nums1, 2, nums2, 2);
+    printf("meddlevalue %lf",result);
+    return 0;
+};
 //70. Climbing Stairs
 //You are climbing a stair case. It takes n steps to reach to the top.
 //Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
