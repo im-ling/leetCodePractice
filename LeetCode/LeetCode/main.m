9e64d5259ce128cd2eada2407bf005f12b8a5ac1
DoubleL <tobeabetterman_dl@163.com>
2018-11-24 20:29:38 +0800

    //3. Longest Substring Without Repeating Characters

--- LeetCode/LeetCode/main.m
+++ LeetCode/LeetCode/main.m
@@ -16,6 +16,8 @@ int main(int argc, const char * argv[]) {
 //        question1();
 //        int question2(void);
 //        question2();
+//        int question3(void);
+//        question3();
 //        int question70(void);
 //        question70();
     }
@@ -127,6 +129,91 @@ int question2(void){
     }
     return 0;
 }
+
+
+
+
+//3. Longest Substring Without Repeating Characters
+//Medium
+//Given a string, find the length of the longest substring without repeating characters.
+//Example 1:
+//Input: "abcabcbb"
+//Output: 3
+//Explanation: The answer is "abc", with the length of 3.
+//Example 2:
+//Input: "bbbbb"
+//Output: 1
+//Explanation: The answer is "b", with the length of 1.
+//Example 3:
+//Input: "pwwkew"
+//Output: 3
+//Explanation: The answer is "wke", with the length of 3.
+//Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
+
+
+// my
+//int findMaxString(char *s, int *max){
+//    int charlist[128] = {0};
+//    printf("sizeof charlist %lu",sizeof(charlist));
+//    memset(charlist, 0, sizeof(charlist));
+//    int result = 0;
+//    char *p = s;
+//    int continueFlag = 1;
+//    while (*p != '\0' && continueFlag) {
+//        if (charlist[*p] == 0) {
+////            printf("can enter?");
+//            result++;
+//            charlist[*p] = result;
+//        }else{
+//            continueFlag = false;
+//            break;
+//        }
+//        p++;
+//    }
+//    if(result > *max)
+//        *max = result;
+//    if (charlist[*p] == 0) {
+//        return 1;
+//    }
+//    return charlist[*p];
+//}
+//
+//int lengthOfLongestSubstring(char* s) {
+//    char *p = s;
+//    int max = 0;
+//    while (NULL != p && *p != '\0') {
+////        printf("\n max(%d) string(%s) %p",max,p,p);
+//        p += findMaxString(p, &max);
+//    }
+//    return max;
+//}
+
+// one of the best answer
+//思想：i为当前报数，j为之前重复的index的最大；i-j+1为当前值的无重复个数
+int lengthOfLongestSubstring(char* s)
+{
+    int i,j=0;
+    int length = (int)strlen(s);
+    int ans = 0;
+    int bucket[128];
+    for (i=0;i<127;i++) bucket[i]=-1;
+        if(length<=1) return length;
+
+    for (i=0;i<length;i++){
+        if (bucket[s[i]]<0) bucket[s[i]]=i;   //若标志位没置，说明没有使用过
+        else{
+            if (j<bucket[s[i]]+1) j=bucket[s[i]]+1; //j为重复值的上一个index，若出现重复值，然而其index不如j的话，那就没必要再更新j了，因为j的index已经重复了
+            bucket[s[i]]=i;
+        }
+        if(i-j+1>ans) ans=i-j+1;
+    }
+    return ans;
+}
+
+int question3(){
+    printf("maxleng : %d",lengthOfLongestSubstring("bacdefahijklmnokpb"));
+    return 0;
+};
 
 //70. Climbing Stairs
 //You are climbing a stair case. It takes n steps to reach to the top.
