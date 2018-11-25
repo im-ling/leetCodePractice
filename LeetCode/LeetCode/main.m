d32eba237c6ab8e11002aa86fb7a3737d43a2cf0
DoubleL <tobeabetterman_dl@163.com>
2018-11-25 13:57:43 +0800

    //5. Longest Palindromic Substring

--- LeetCode/LeetCode/main.m
+++ LeetCode/LeetCode/main.m
@@ -20,6 +20,8 @@ int main(int argc, const char * argv[]) {
 //        question3();
 //        int question4(void);
 //        question4();
+//        int question5(void);
+//        question5();
 //        int question70(void);
 //        question70();
     }
@@ -273,6 +275,106 @@ int question4(){
     printf("meddlevalue %lf",result);
     return 0;
 };
+
+//5. Longest Palindromic Substring
+//Medium
+//Given a string s, find the longest palindromic substring in s. You may assume that the maximum length of s is 1000.
+//Example 1:
+//Input: "babad"
+//Output: "bab"
+//Note: "aba" is also a valid answer.
+//Example 2:
+//Input: "cbbd"
+//Output: "bb"
+
+int checkIsOddPalindrome(char *s, char *first, char *last, int *maxLength,char **target, bool *isOdd){
+    char *pLeft = s - 1;
+    char *pRight= s + 1;
+    int result = 0;
+    *isOdd = false;
+    while (pLeft >= first && pRight <= last) {
+        if (*pLeft == *pRight) {
+            result++;
+        }else{
+            break;
+        }
+        pLeft--;
+        pRight++;
+    }
+    if (result * 2 + 1 > *maxLength && result > 0) {
+        *maxLength = result * 2 + 1;
+        *target = s - result;
+    }
+    if (result > 0) {
+        *isOdd = true;
+    }
+    return result;
+}
+
+int checkIsEvenPalindrome(char *s, char *first, char *last, int *maxLength,char **target, bool *isEven){
+    char *pLeft = s;
+    char *pRight= s + 1;
+    int result = 0;
+    *isEven = false;
+    while (pLeft >= first && pRight <= last) {
+        if (*pLeft == *pRight) {
+            result++;
+        }else{
+            break;
+        }
+        pLeft--;
+        pRight++;
+    }
+    if (result * 2 > *maxLength && result > 0) {
+        *maxLength = result * 2;
+        *target = s - result + 1;
+    }
+    if (result > 0) {
+        *isEven = true;
+    }
+    return result;
+}
+
+char* longestPalindrome(char* s) {
+    char *target = s;
+    char *p = s;
+    char *last = s + strlen(s);
+    int maxLength = 0;
+    
+    bool isOdd = false;
+    bool isEven = false;
+    
+//    int step;
+//    int step1;
+//    int step2;
+    
+    while (p>=s && p<= last) {
+        printf("\ncurrent s(%s) target(%s) maxleng(%d)",p,target ,maxLength);
+        checkIsOddPalindrome(p, s, last, &maxLength, &target, &isOdd);
+        checkIsEvenPalindrome(p, s, last, &maxLength, &target, &isEven);
+//        step = step1 > step2 ? step1 : step2;
+//        p += step;
+        p++;
+    }
+    if (maxLength == 0) {
+        char *result = malloc(sizeof(char) * 2);
+        *result = *s;
+        *(result + 1) = '\0';
+        return result;
+    }
+    char *result = malloc(sizeof(char) * (maxLength + 1));
+    memset(result, 0, sizeof(char) * (maxLength + 1));
+    memcpy(result, target, maxLength);
+    result[maxLength] = '\0';
+    return result;
+}
+int question5(){
+    char *s = "aaaaa";
+    printf("\nlongest %s",longestPalindrome(s));
+    printf("\nfinished");
+    return 0;
+};
+
 //70. Climbing Stairs
 //You are climbing a stair case. It takes n steps to reach to the top.
 //Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
