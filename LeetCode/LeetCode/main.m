2c94a0a93c826476429e179a4de8a75cd419df01
DoubleL <tobeabetterman_dl@163.com>
2018-12-03 20:11:37 +0800

    //23. Merge k Sorted Lists

--- LeetCode/LeetCode/main.m
+++ LeetCode/LeetCode/main.m
@@ -56,7 +56,9 @@ int main(int argc, const char * argv[]) {
 //        question21();
 //        int question22(void);
 //        question22();
-
+//        int question23(void);
+//        question23();
+        
 //        int question70(void);
 //        question70();
     }
@@ -1909,6 +1911,11 @@ int question20(){
 //    return result;
 //}
 
+
+//struct ListNode {
+//    int val;
+//    struct ListNode *next;
+//};
 struct ListNode* mergeTwoLists(struct ListNode* l1, struct ListNode* l2) {
     if (l1 == NULL) {
         return l2;
@@ -1923,10 +1930,7 @@ struct ListNode* mergeTwoLists(struct ListNode* l1, struct ListNode* l2) {
     }
 }
 
-//struct ListNode {
-//    int val;
-//    struct ListNode *next;
-//};
+
 
 int question21(){
     int listSize = 4;
@@ -2034,6 +2038,185 @@ int question22(){
     return 0;
 }
 
+
+
+
+//23. Merge k Sorted Lists
+//Hard
+//1746
+//114
+//
+//
+//Merge k sorted linked lists and return it as one sorted list. Analyze and describe its complexity.
+//
+//Example:
+//
+//Input:
+//[
+// 1->4->5,
+// 1->3->4,
+// 2->6
+// ]
+//Output: 1->1->2->3->4->4->5->6
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+
+int printList(struct ListNode* list){
+    printf("\n printLists:\n");
+    while (list != NULL) {
+        printf(" %d",list->val);
+        list = list->next;
+    }
+    return 0;
+}
+int printTwoDimensionLists(struct ListNode** lists, int listsSize){
+    printf("\n printTwoDimensionLists:");
+    for (int i = 0; i < listsSize; i++) {
+        printf("\n i(%d)",i);
+        struct ListNode* p = lists[i];
+        while (p != NULL) {
+            printf(" %d",p->val);
+            p = p->next;
+        }
+    }
+    return 0;
+}
+struct ListNode* intArrayToList(int *array, int number){
+    struct ListNode* result = calloc(1, sizeof(struct ListNode));
+    struct ListNode* p = result;
+    for (int i = 0; i < number; i++) {
+        p->next = calloc(1, sizeof(struct ListNode));
+        p->next->val = array[i];
+        p = p->next;
+    }
+    p = result;
+    result = result->next;
+    free(p);
+    return result;
+}
+
+//// 68 ms, faster than 63.11%
+//struct ListNode* mergeKLists(struct ListNode** lists, int listsSize) {
+//    struct ListNode *result = calloc(1, sizeof(struct ListNode));
+//    struct ListNode *p = result;
+////    struct ListNode *pTmp = NULL;
+//    int indexMin = 0;
+//    int min = INT32_MAX;
+//    int secondMin = INT32_MAX;
+//    while (listsSize > 0) {
+//        bool findFlag = false;
+//        for (int i = 0; i < listsSize; i++) {
+//            if (lists[i] == NULL) {
+//                if (listsSize <= 1) {
+//                    listsSize--;
+//                }else{
+//                    lists[i] = lists[--listsSize];
+//                }
+//                i--;
+//            }else if (min > lists[i]->val){
+//                    findFlag = true;
+//                    indexMin = i;
+//                    secondMin = min;
+//                    min = lists[i]->val;
+//            }else if(secondMin > lists[i]->val){
+//                secondMin = lists[i]->val;
+//            }
+//        }
+//        if (findFlag) {
+//            p->next = lists[indexMin];
+////            printf("\n i(%d) val(%d) ", indexMin, p->val);
+//            p = p->next;
+//            lists[indexMin] = lists[indexMin]->next;
+//            while (lists[indexMin] && secondMin != INT32_MAX && lists[indexMin]->val < secondMin) {
+//                p = lists[indexMin];
+//                lists[indexMin] = lists[indexMin]->next;
+//            }
+//            min = INT32_MAX;
+//            secondMin = INT32_MAX;
+//            findFlag = false;
+//        }
+////        printTwoDimensionLists(lists, listsSize);
+////        printList(result->next);
+//    }
+//    p = result;
+//    result = result->next;
+//    free(p);
+//    return result;
+//}
+
+// 148 ms why it takes so long ?
+//struct ListNode *mergeKLists(struct ListNode **lists, int listsSize) {
+//    if (listsSize == 0)  {
+//        return NULL;
+//    }
+//    if (listsSize == 1)   {
+//        return lists[0];
+//    }
+//    for (int i = listsSize - 1; i > 0; i--) {
+//        lists[0] = mergeTwoLists(lists[0], lists[i]);
+//    }
+//    return lists[0];
+//}
+
+////8 ms !!!
+struct ListNode *mergeKLists(struct ListNode **lists, int listsSize) {
+    if (listsSize == 0)  {
+        return NULL;
+    }
+    if (listsSize == 1)   {
+        return lists[0];
+    }
+    for (int i = 0,j = listsSize - 1; i < j; i++, j--) {
+        lists[i] = mergeTwoLists(lists[i], lists[j]);
+        listsSize--;
+    }
+    return mergeKLists(lists, listsSize);
+}
+
+int question23(){
+    int listSize = 3;
+    struct ListNode** lists = calloc(listSize, sizeof(struct ListNode*));
+    int array[3][4] = {{3,1,4,5},{3,1,3,4},{2,2,6}};;
+    for (int i = 0; i < 3; i++) {
+        lists[i] = intArrayToList(array[i] + 1, array[i][0]);
+//        printList(lists[i]);
+    }
+    printTwoDimensionLists(lists,listSize);
+//    for (int i = 0; i < listSize; i++) {
+//        lists[i] = calloc(1, sizeof(struct ListNode));
+//        struct ListNode *p = lists[i];
+//        printf("\n");
+//        printf(" %d ",p->val);
+//        for (int j = 0; j < listSize - 1; j++) {
+//            p->next = calloc(1, sizeof(struct ListNode));
+//            p->next->val = j * listSize + i;
+//            printf(" %d ",p->next->val);
+//            p = p->next;
+//        }
+//    }
+//
+//    printf("\n result:");
+//    for (int i = 0; i < listSize; i++) {
+//        struct ListNode *p = lists[i];
+//        printf("\n i(%d) ",i);
+//        while (p != NULL) {
+//            printf(" %d ", p->val);
+//            p = p->next;
+//        }
+//    }
+
+    struct ListNode* result = mergeKLists(lists, listSize);
+    printf("\nresult\n");
+    printList(result);
+    
+    return 0;
+}
 
 //70. Climbing Stairs
 //You are climbing a stair case. It takes n steps to reach to the top.
