fd268727faf53e4e196e9a145e27645cf9d5b175
DoubleL <tobeabetterman_dl@163.com>
2018-12-03 21:20:33 +0800

    //25. Reverse Nodes in k-Group

--- LeetCode/LeetCode/main.m
+++ LeetCode/LeetCode/main.m
@@ -60,6 +60,8 @@ int main(int argc, const char * argv[]) {
 //        question23();
 //        int question24(void);
 //        question24();
+//        int question25(void);
+//        question25();
 
 //        int question70(void);
 //        question70();
@@ -2070,8 +2072,12 @@ int question22(){
  */
 
 int printList(struct ListNode* list){
-    printf("\n printLists:\n");
+    printf("\n printLists:");
+    int count = 0;
     while (list != NULL) {
+        if (count++ %5 == 0) {
+            printf("\n");
+        }
         printf(" %d",list->val);
         list = list->next;
     }
@@ -2265,6 +2271,86 @@ int question24(){
     return 0;
 }
 
+//25. Reverse Nodes in k-Group
+//Hard
+//808
+//182
+//
+//
+//Given a linked list, reverse the nodes of a linked list k at a time and return its modified list.
+//
+//k is a positive integer and is less than or equal to the length of the linked list. If the number of nodes is not a multiple of k then left-out nodes in the end should remain as it is.
+//
+//Example:
+//
+//Given this linked list: 1->2->3->4->5
+//
+//For k = 2, you should return: 2->1->4->3->5
+//
+//For k = 3, you should return: 3->2->1->4->5
+//
+//Note:
+//
+//Only constant extra memory is allowed.
+//You may not alter the values in the list's nodes, only nodes itself may be changed.
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode* reverseKGroup(struct ListNode* head, int k) {
+    if (head == NULL) {
+        return head;
+    }
+    struct ListNode **lists = calloc(k, sizeof(struct ListNode *));
+    struct ListNode *p = head;
+    struct ListNode *lastTail = NULL;
+    struct ListNode *result = NULL;
+    int count = 0;
+    while (p != NULL) {
+        lists[count++] = p;
+        p = p->next;
+        if (count == k) {
+            
+            if (lastTail) {
+                lastTail->next = lists[k-1];
+            }else if (result == NULL){
+                result = lists[count - 1];
+            }
+            for (int i = k - 1; i > 0 ; i--) {
+                lists[i]->next = lists[i - 1];
+            }
+            lastTail = lists[0];
+            count = 0;
+//            printList(result);
+        }
+    }
+    if (count) {
+        if (lastTail) {
+            lastTail->next = lists[0];
+        }
+        if (result == NULL) {
+            result = lists[0];
+        }
+    }else{
+        lists[0]->next = NULL;
+    }
+    return result;
+}
+int question25(){
+    int size = 1;
+    int testArray[size];
+    for (int i = 0; i < size; i++) {
+        testArray[i] = i + 1;
+    }
+    struct ListNode *result = intArrayToList(testArray, size);
+    printList(result);
+    result = reverseKGroup(result, 2);
+    printList(result);
+    return 0;
+}
 
 //70. Climbing Stairs
 //You are climbing a stair case. It takes n steps to reach to the top.
