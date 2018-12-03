3859784b975f3712fb59bfc4d43e2be174384588
DoubleL <tobeabetterman_dl@163.com>
2018-12-03 20:32:25 +0800

    //24. Swap Nodes in Pairs

--- LeetCode/LeetCode/main.m
+++ LeetCode/LeetCode/main.m
@@ -58,7 +58,9 @@ int main(int argc, const char * argv[]) {
 //        question22();
 //        int question23(void);
 //        question23();
-        
+//        int question24(void);
+//        question24();
+
 //        int question70(void);
 //        question70();
     }
@@ -2217,6 +2219,52 @@ int question23(){
     
     return 0;
 }
+
+//24. Swap Nodes in Pairs
+//Medium
+//789
+//65
+//
+//
+//Given a linked list, swap every two adjacent nodes and return its head.
+//
+//Example:
+//
+//Given 1->2->3->4, you should return the list as 2->1->4->3.
+//Note:
+//
+//Your algorithm should use only constant extra space.
+//You may not modify the values in the list's nodes, only nodes itself may be changed.
+struct ListNode* swapPairs(struct ListNode* head) {
+    if (head == NULL || head->next == NULL) {
+        return head;
+    }
+    struct ListNode *result = head->next;
+    struct ListNode *p = head;
+    struct ListNode *tmpNode = NULL;
+    struct ListNode *pLast = NULL;
+    while (p != NULL && p->next != NULL) {
+//        printList(p);
+        if (pLast) {
+            pLast->next = p->next;
+        }
+        tmpNode = p->next->next;
+        p->next->next = p;
+        p->next = tmpNode;
+        pLast = p;
+        p = p->next;
+    }
+    return result;
+}
+
+int question24(){
+    int testArray[] = {1,2,3,4,5,6,7};
+    struct ListNode *test = intArrayToList(testArray, 7);
+    struct ListNode *result = swapPairs(test);
+    printList(result);
+    return 0;
+}
+
 
 //70. Climbing Stairs
 //You are climbing a stair case. It takes n steps to reach to the top.
