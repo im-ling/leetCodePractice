4053974df4080cee44ab7b678843fa86a4606498
DoubleL <tobeabetterman_dl@163.com>
2018-12-16 19:22:09 +0800

    //35. Search Insert Position

--- LeetCode/LeetCode/main.m
+++ LeetCode/LeetCode/main.m
@@ -81,6 +81,8 @@ int main(int argc, const char * argv[]) {
 //        question33();
 //        int question34(void);
 //        question34();
+//        int question35(void);
+//        question35();
 //        int question70(void);
 //        question70();
     }
@@ -3091,6 +3093,64 @@ int question34(){
     printf("\n");
     return 0;
 }
+
+
+//35. Search Insert Position
+//Easy
+//
+//1014
+//
+//148
+//
+//Favorite
+//
+//Share
+//Given a sorted array and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+//
+//You may assume no duplicates in the array.
+//
+//Example 1:
+//
+//Input: [1,3,5,6], 5
+//Output: 2
+//Example 2:
+//
+//Input: [1,3,5,6], 2
+//Output: 1
+//Example 3:
+//
+//Input: [1,3,5,6], 7
+//Output: 4
+//Example 4:
+//
+//Input: [1,3,5,6], 0
+//Output: 0
+//Accepted
+//331,155
+//Submissions
+//825,733
+
+
+int searchInsert(int* nums, int numsSize, int target) {
+    int result = 0;
+    int *lastLeft = searchLastLeft(nums, numsSize, target);
+    result = (int)(lastLeft - nums + 1);
+    if (*lastLeft >= target) {
+        result--;
+    }
+    return result;
+}
+
+int question35(){
+    int nums[4] = {1,3,5,6};
+    for (int i = 0; i < sizeof(nums) / sizeof(int); i++) {
+        printf(" %d",nums[i]);
+    }
+    int result = searchInsert(nums, sizeof(nums) / sizeof(int), 4);
+    printf("\n result(%d)",result);
+    return 0;
+}
+
 //70. Climbing Stairs
 //You are climbing a stair case. It takes n steps to reach to the top.
 //Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
