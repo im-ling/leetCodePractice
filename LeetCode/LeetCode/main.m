9c417238dec03f8e4aa39f8010df1a3804933616
DoubleL <tobeabetterman_dl@163.com>
2019-02-19 16:12:15 +0800

    question 42

--- LeetCode/LeetCode/main.m
+++ LeetCode/LeetCode/main.m
@@ -91,6 +91,9 @@ int main(int argc, const char * argv[]) {
 //        question38();
 //        int question41(void);
 //        question41();
+//        int question42(void);
+//        question42();
+
 
 //        int question70(void);
 //        question70();
@@ -3568,6 +3571,20 @@ int question38(void){
 //Output: 3
 //Example 2:
 //
+//Input: [3,4,-1,1]
+//Output: 2
+//Example 3:
+//
+//Input: [7,8,9,11,12]
+//Output: 1
+//Note:
+//
+//Your algorithm should run in O(n) time and uses constant extra space.
+//
+//Accepted
+//187,760
+//Submissions
+//668,756
 
 int firstMissingPositive(int* nums, int numsSize) {
     /**
@@ -3610,22 +3627,92 @@ int question41(){
     printf("first missing positive number is %d",result);
     return 0;
 }
-//Input: [3,4,-1,1]
-//Output: 2
-//Example 3:
+
+//42. Trapping Rain Water
+//Hard
 //
-//Input: [7,8,9,11,12]
-//Output: 1
-//Note:
+//2918
 //
-//Your algorithm should run in O(n) time and uses constant extra space.
+//51
 //
+//Favorite
+//
+//Share
+//Given n non-negative integers representing an elevation map where the width of each bar is 1, compute how much water it is able to trap after raining.
+//
+//
+//The above elevation map is represented by array [0,1,0,2,1,0,1,3,2,1,2,1]. In this case, 6 units of rain water (blue section) are being trapped. Thanks Marcos for contributing this image!
+//
+//Example:
+//
+//Input: [0,1,0,2,1,0,1,3,2,1,2,1]
+//Output: 6
 //Accepted
-//187,760
+//252,295
 //Submissions
-//668,756
-
-
+//605,652
+int trap(int* height, int heightSize) {
+    if (heightSize <= 1) {
+        return 0;
+    }
+    int result = 0;
+    int *newHeights = calloc(heightSize, sizeof(int));
+    memcpy(newHeights, height, heightSize * sizeof(int));
+    qsort(newHeights, heightSize, sizeof(int), compareIntFunction);
+    int count = 1;
+    for (int i = 1; i < heightSize; i++) {
+        if (newHeights[i] > newHeights[i - 1]) {
+            //            printf("\nnewheight[%d] = %d",count, newHeights[i]);
+            newHeights[count++] = newHeights[i];
+        }
+    }
+    
+    int lastLeft = heightSize - 1;
+    int lastRight = 0;
+    int firstTimeFlag = true;
+    for (int i = count - 1; i >= 0; i--) {
+        //        printf("\ncurretn number %d result %d", newHeights[i], result);
+        int left = 0;
+        int right = heightSize - 1;
+        while (left < heightSize && height[left++] < newHeights[i]) {
+        }
+        while (right >= 0 && height[right--] < newHeights[i]) {
+        }
+        left--;
+        right++;
+        if (firstTimeFlag) {
+            firstTimeFlag = false;
+            for (int j = left; j < right; j++) {
+                if (newHeights[i] > height[j]) {
+                    result = result + newHeights[i] - height[j];
+                }
+            }
+        }else{
+            for (int j = left; j < lastLeft; j++) {
+                if (newHeights[i] > height[j]) {
+                    result = result + newHeights[i] - height[j];
+                }
+            }
+            for (int j = right; j > lastRight; j--) {
+                if (newHeights[i] > height[j]) {
+                    result = result + newHeights[i] - height[j];
+                }
+            }
+        }
+        lastLeft = left;
+        lastRight = right;
+    }
+    return result;
+}
+int question42(){
+    int nums[] =
+    {4,2,3};
+    //    {2,0,2};
+    //    {0,1,0,2,1,0,1,3,2,1,2,1};
+    int result = trap(nums, sizeof(nums) / sizeof(nums[0]));
+    printf("\ntrap %d",result);
+    return 0;
+}
 
 
 //70. Climbing Stairs
