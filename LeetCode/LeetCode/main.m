9cec997f21ea7af9d82aaa6bc5dd643d502d9250
liling <liling@sjdd.com.cn>
2019-08-16 09:30:03 +0800

    feat: add q86 Partition List

--- LeetCode/LeetCode/main.m
+++ LeetCode/LeetCode/main.m
@@ -105,6 +105,8 @@ int main(int argc, const char * argv[]) {
 //        question75();
 //        int question80(void);
 //        question80();
+//        int question86(void);
+//        question86();
     }
     return 0;
 }
@@ -4308,5 +4310,96 @@ int question80(){
     printf("\n");
 
     
+    return 0;
+}
+
+
+//86. Partition List
+//Medium
+//760
+//204
+//Favorite
+//
+//Share
+//Given a linked list and a value x, partition it such that all nodes less than x come before nodes greater than or equal to x.
+//
+//You should preserve the original relative order of the nodes in each of the two partitions.
+//
+//Example:
+//
+//Input: head = 1->4->3->2->5->2, x = 3
+//Output: 1->2->2->4->3->5
+//Accepted
+//173,663
+//Submissions
+//456,206
+
+//struct ListNode* partition(struct ListNode* head, int x){
+//    struct ListNode preHead = {INT_MIN,head};
+//    struct ListNode* pFast = &preHead;
+//    struct ListNode* pSlow = &preHead;
+//    struct ListNode* temp = &preHead;
+//    bool isFirstGreater = false;
+//    while (pFast->next != NULL) {
+//        if (pFast->next->val < x) {
+//            if (isFirstGreater) {
+//                temp = pFast->next;
+//                pFast->next = temp->next;
+//                temp->next = pSlow->next;
+//                pSlow->next = temp;
+//            }else{
+//                pFast = pFast->next;
+//            }
+//            pSlow = pSlow->next;
+//        }else{
+//            isFirstGreater = true;
+//            pFast = pFast->next;
+//        }
+//    }
+//    return preHead.next;
+//}
+
+
+struct ListNode* partition(struct ListNode* head, int x){
+    struct ListNode nodeMin = {INT_MIN,NULL};
+    struct ListNode nodeMax = {INT_MAX,NULL};
+    struct ListNode* pNodeMin = &nodeMin;
+    struct ListNode* pNodeMax = &nodeMax;
+
+    struct ListNode* p = head;
+    while (p) {
+        if (p->val < x) {
+            pNodeMin->next = p;
+            pNodeMin = pNodeMin->next;
+        }else{
+            pNodeMax->next = p;
+            pNodeMax = pNodeMax->next;
+        }
+        p = p->next;
+    }
+    pNodeMin->next = nodeMax.next;
+    pNodeMax->next = NULL;
+    return nodeMin.next;
+}
+
+struct ListNode* initANodeListWithLenAndThreshold(int listLen, int threshold){
+    struct ListNode headNode = {0, NULL};
+    struct ListNode *head =  &headNode;
+    for (int i = listLen; i > 0; i--) {
+        struct ListNode *newNode = calloc(1, sizeof(struct ListNode));
+        newNode->val = arc4random() % threshold;
+        newNode->next = head->next;
+        head->next = newNode;
+    }
+    return head->next;
+}
+
+int question86(){
+    int listLen = 10;
+    int threshold = listLen / 2;
+    struct ListNode *list1 = initANodeListWithLenAndThreshold(listLen, listLen);
+    printListWithThreshold(list1, listLen);
+    list1 = partition(list1, threshold);
+    printListWithThreshold(list1, listLen);
     return 0;
 }
