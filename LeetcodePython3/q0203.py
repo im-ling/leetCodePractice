83b1eb7da5792a909ae10c0c14c781a13c84db61
NowOrNever <tobeabetterman_dl@163.com>
2021-06-06 13:01:20 +0800

    refactor: modify 203

--- LeetcodePython3/q0203.py
+++ LeetcodePython3/q0203.py
@@ -2,15 +2,14 @@ class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
+
+
 class Solution:
     def removeElements(self, head: ListNode, val: int) -> ListNode:
         preHead = ListNode(0, head)
-        p_pre = preHead
-        p = head
-        while p:
-            while p.val != val:
-                p = p.next
-                p_pre = p_pre.next
-            p_pre.next = p.next
+        p = preHead
+        while p.next:
+            if p.next.val == val:
+                p.next = p.next.next
             p = p.next
         return preHead.next
