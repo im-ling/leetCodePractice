d7ff53605ee465bdc6f7afd4665c4304a11d0f3f
NowOrNever <tobeabetterman_dl@163.com>
2021-08-06 13:28:19 +0800

    feat: rewrite q0198

---
+++ LeetcodePython3/q0198.py
@@ -0,0 +1,9 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        length = len(nums)
+        dp = [[0, 0] for _ in range(length)]
+        dp[0][0] = nums[0]
+        for i in range(1, length):
+            dp[i][1] = max(dp[i - 1][1], dp[i - 1][0])
+            dp[i][0] = dp[i - 1][1] + nums[i]
+        return max(dp[length - 1][0], dp[length - 1][1])
