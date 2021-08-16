016abcf51fe431876bbdb2cd8726da484bfb0acc
NowOrNever <tobeabetterman_dl@163.com>
2021-08-16 15:10:04 +0800

    refactor: rewrite q0300

--- LeetcodePython3/q0300.py
+++ LeetcodePython3/q0300.py
@@ -4,12 +4,41 @@ from typing import List
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
         length = len(nums)
-        dp = [1 for _ in range(length)]
-        result = 0
+        stack = [0 for _ in range(length)]
+        stack[0] = nums[0]
+        count = 1
+
         for i in range(1, length):
-            for j in reversed(range(0, i)):
-                if nums[i] > nums[j]:
-                    dp[i] = max(dp[i], dp[j] + 1)
-                    if dp[i] > result:
-                        result = dp[i]
-        return result
+            num = nums[i]
+
+            left = 0
+            right = count - 1
+            index = -1
+            while left <= right:
+                mid = (left + right) >> 1
+                if num > stack[mid]:
+                    left = mid + 1
+                    index = mid
+                else:
+                    right = mid - 1
+            index += 1
+            stack[index] = num
+            if index == count:
+                count += 1
+        return count
+
+
+nums = [10,9,2,5,3,7,101,18]
+result = Solution().lengthOfLIS(nums)
+print(result)
+    # def lengthOfLIS(self, nums: List[int]) -> int:
+    #     length = len(nums)
+    #     dp = [1 for _ in range(length)]
+    #     result = 0
+    #     for i in range(1, length):
+    #         for j in reversed(range(0, i)):
+    #             if nums[i] > nums[j]:
+    #                 dp[i] = max(dp[i], dp[j] + 1)
+    #                 if dp[i] > result:
+    #                     result = dp[i]
+    #     return result
