4cc78542cdafb8a711b36f9f530af19ad95bcdd4
Ling <tobeabetterman_dl@163.com>
2022-08-15 16:32:23 +0800

    feat: update code for q0705

--- LeetcodePython3/q0705.py
+++ LeetcodePython3/q0705.py
@@ -7,56 +7,50 @@ class MyHashSet:
     hashList = list()
 
     def __init__(self):
-        """
-        Initialize your data structure here.
-        """
         self.hashList = [-1 for _ in range(self.prime)]
 
     def add(self, key: int) -> None:
-        left = key % self.prime
-        while self.hashList[left] != -1 and self.hashList[left] != key:
-            left = (1 + left) % self.prime
-        if self.hashList[left] == key:
+        index = key % self.prime
+        while self.hashList[index] != -1 and self.hashList[index] != key:
+            index = (1 + index) % self.prime
+        if self.hashList[index] == key:
             return
         else:
-            self.hashList[left] = key
-
-    def remove(self, key: int) -> None:
-        startIndex = key % self.prime
-        left = startIndex
-        while self.hashList[left] != -1 and self.hashList[left] != key:
-            left = (1 + left) % self.prime
-        if self.hashList[left] == -1:
-            return
-        else:
-            self.hashList[left] = -1
-            right = (left + 1) % self.prime
-            while self.hashList[right] != -1:
-                value = self.hashList[right]
-                self.hashList[right] = -1
-                self.add(value)
-                right += 1
+            self.hashList[index] = key
 
     def contains(self, key: int) -> bool:
-        """
-        Returns true if this set contains the specified element
-        """
-        left = key % self.prime
-        while self.hashList[left] != -1 and self.hashList[left] != key:
-            left = (1 + left) % self.prime
-        if self.hashList[left] == key:
+        index = key % self.prime
+        while self.hashList[index] != -1 and self.hashList[index] != key:
+            index = (1 + index) % self.prime
+        if self.hashList[index] == key:
             return True
         else:
             return False
 
+    def remove(self, key: int) -> None:
+        index = key % self.prime
+        while self.hashList[index] != -1 and self.hashList[index] != key:
+            index = (1 + index) % self.prime
+        if self.hashList[index] == -1:
+            # 没找到退出
+            return
+        else:
+            # 找到了删除
+            self.hashList[index] = -1
+            index = (index + 1) % self.prime
+            while self.hashList[index] != -1:
+                origin = self.hashList[index]
+                self.hashList[index] = -1
+                self.add(origin)
+                index += 1
+
+
 # Your MyHashSet object will be instantiated and called as such:
 obj = MyHashSet()
+obj.add(0)
 obj.add(1)
-obj.add(2)
 print(obj.contains(1))
 print(obj.contains(3))
-obj.add(2)
-
-
+obj.add(19993)
 
 # param_3 = obj.contains(key)
